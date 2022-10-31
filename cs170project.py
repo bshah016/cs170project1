65edda4f790a144cb994f9918f58eadd2a7a2ea4
Brij Shah <56499450+bshah016@users.noreply.github.com>
2022-10-30 19:15:31 -0700

    Update cs170project.py

--- cs170project.py
+++ cs170project.py
@@ -1,8 +1,16 @@
 
+#https://docs.python.org/3/howto/sorting.html
+#https://docs.python.org/3/library/functions.html#zip
+#^ Used these as a reference, but they didn't seem too useful compared to what I found on stack overflow
+#https://www.geeksforgeeks.org/python-program-for-heap-sort/
+#^ Tried to use this to sort the nodes list so the nodes list could be sorted based off of the combo of h(n) and g(n), but again, didn't really work
+
 #define your solution states here
 import queue
 import copy
 from shutil import move
+import pyfiglet
+from termcolor import colored
 
 
 EIGHT = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
@@ -15,32 +23,54 @@ def main():
     choice = 5
     problem = []
     while choice != 0:
-        print("Welcome to my 8-Puzzle Solver. Type '1' to use a default puzzle, or '2' to create your own, or 0 to quit")
+        # result = pyfiglet.figlet_format("My  8\nPuzzle\nSolver", font = "doh", width = 5000)
+        result = pyfiglet.figlet_format("My  8", font = "banner3-D")
+        print(colored(result, 'blue'))
+        result = pyfiglet.figlet_format("Puzzle", font = "banner3-D")
+        print(colored(result, 'green'))
+        result = pyfiglet.figlet_format("Solver", font = "banner3-D")
+        print(colored(result, 'yellow'))
+        print("\nType: \n(1) to use a default puzzle \n(2) to create your own \n(0) to quit\nChoice: ")
         choice = int( input() )
         if choice == 1:
-            problem = [[1, 3, 6], [5, 0, 2], [4, 7, 8]]
+            problem = [[0, 1, 3], [4, 2, 5], [7, 8, 6]]
             choice = 0
         elif choice == 2:
-            print("Enter your puzzle, using a zero to represent the blank.")
-            print("Please only enter valid 8-puzzles.")
-            print("Enter the puzzle demilimiting the numbers with a space. (0 indicates the blank space)")
-            print("Type RETURN only when finished with the row.")
+            print("------------------------------------------------------------\n")
+            print("Enter your puzzle, using a zero to represent the blank.\n")
+            print("Please only enter valid 8-puzzles.\n")
+            print("Enter the puzzle demilimiting the numbers with a space. (0 indicates the blank space)\n")
+            print("Type RETURN only when finished with the row.\n")
+            print("------------------------------------------------------------\n")
 
-            firstRow = input(print("First Row:"))
-            secondRow = input(print("Second Row:"))
-            thirdRow = input(print("Third Row:"))
-
-            firstRow.split(' ')
-            secondRow.split(' ')
-            thirdRow.split(' ')
-
+            print("First Row:")
+            firstRow = input()
+            print("Second Row:")
+            secondRow = input()
+            print("Third Row:")
+            thirdRow = input()
+            list(firstRow)
+            list(secondRow)
+            list(thirdRow)
+            #https://stackoverflow.com/questions/2186656/how-can-i-remove-all-instances-of-an-element-from-a-list-in-python
+            firstRow = [x for x in firstRow if x != ' ']
+            secondRow = [x for x in secondRow if x != ' ']
+            thirdRow = [x for x in thirdRow if x != ' ']
+            #not sure why I even looked it up, but I did reference it, so I'm going to put it in just in case lol
+            #https://www.geeksforgeeks.org/python-converting-all-strings-in-list-to-integers/
+            for i in range(len(firstRow)):
+                firstRow[i] = int(firstRow[i])
+                secondRow[i] = int(secondRow[i])
+                thirdRow[i] = int(thirdRow[i])
+            # print(len(firstRow))
             problem = [firstRow, secondRow, thirdRow]
+            # print(problem)
             choice = 0
         else:
             print("Please enter a valid choice")
 
     # print(problem)
-    algNum = input(print("Select algorithm. (1) for Uniform Cost Search, (2) for the Misplaced Tile Heuristic, or (3) the Manhattan Distance Heuristic"))
+    algNum = input("Select algorithm. \n (1) Uniform Cost Search \n (2) Misplaced Tile Heuristic \n (3) Manhattan Distance Heuristic\nChoice: ")
     qfunct = int( algNum )
     print(generalsearch(problem, qfunct))
 
@@ -73,12 +103,17 @@ def generalsearch(problem, qfunct):
         nodenum += 1
         node = nodes.pop(0)
         if goal(node):
-            print('Total number of nodes expanded: ' + str(NUM_NODES))
+            print('State to expand has a g(n) of ' + str(node.depth) + ', an h(n) of ' + str(node.hn) + '.\n And it looks like: \n')
+            drawBoard(node.problem)
+            print('\nTotal number of nodes expanded: ' + str(NUM_NODES))
             print('Depth of the node in the tree was: ' + str(node.depth))
             return "Success"
             # return node
-        if NUM_NODES != 0:
-            print('State to expand has a g(n) of ' + str(node.depth) + ', an h(n) of ' + str(node.hn) + '.\n And it looks like: \n' + str(node.problem))
+        # if NUM_NODES != 0:
+        print('State to expand has a g(n) of ' + str(node.depth) + ', an h(n) of ' + str(node.hn) + '.\n And it looks like: \n')
+            # print(str(node.problem))  #un comment this out if you want to print out a higher order puzzle
+            #only for 8 puzzle
+        drawBoard(node.problem) #comment this out if you want to print out a higher order puzzle
         expanded = expand(node, visited, qfunct)
         
         for childnode in expanded:
@@ -113,6 +148,8 @@ def expand(node, visited, qfunct):
         hn = manhattan(node.problem)
     
     if x > 0: #not on top row, so we can move it up
+        #https://realpython.com/copying-python-objects/
+        #we need to deep copy so that way when we edit move_up, we do not change node.problem or node at all
         move_up = copy.deepcopy(node.problem)
         temp = move_up[x][y] #save the original value
         move_up[x][y] = move_up[x - 1][y] #decrement x value by 1 to signal that the space moved up
@@ -209,9 +246,6 @@ def misplaced(problem):
             if int(problem[i][j]) != int(goalstate[i][j]) and int(problem[i][j]) != 0:
                 num_misplaced = num_misplaced + 1
     return num_misplaced
-
-def uniform(problem):
-    return 0
     
 
 def goal(node):
@@ -228,8 +262,34 @@ class Node:
         self.hn = 0
         self.depth = 0
         self.problem = problem
-        self.flag = False
         self.expanded = 0
+
+
+
+#Just something to make it look nicer :)
+#ONLY FOR 8 PUZZLE BECAUSE IT'S A LITTLE HARD TO DO THIS FOR BIGGER ORDER PUZZLES
+#I coded something similar to this when i did a personal project for a tic tac toe program
+#thought it would look pretty cool here as well
+#here's the link just in case: https://github.com/bshah016/CS_Projects/blob/master/TTT.py
+def drawBoard(problem):
+    board_status = []
+    for i in range(len(problem)):
+        for j in range(len(problem)):
+            if int(problem[i][j]) == 0:
+                board_status.append(' ')
+            else:
+                board_status.append(int(problem[i][j]))
+    print('\
+ ╔═══╦═══╦═══╗\n\
+ ║ {0} ║ {1} ║ {2} ║\n\
+ ╠═══╬═══╬═══╣\n\
+ ║ {3} ║ {4} ║ {5} ║\n\
+ ╠═══╬═══╬═══╣\n\
+ ║ {6} ║ {7} ║ {8} ║\n\
+ ╚═══╩═══╩═══╝ '.format(
+               board_status[0], board_status[1], board_status[2], 
+               board_status[3], board_status[4], board_status[5], 
+               board_status[6], board_status[7], board_status[8]))
 
 
 
